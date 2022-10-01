7b92279ed50013e94c430463ed4f8449b5d08c6a
NamitaCODER <103303367+NamitaCODER@users.noreply.github.com>
2022-10-01 13:59:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -81,29 +81,29 @@ function draw() {
   text("EPIC ARCHERY", width / 2, 100);
 }
 
-// function keyPressed() {
-//   if (keyCode === 32) {
-//     var posX = playerArcher.body.position.x;
-//     var posY = playerArcher.body.position.y;
-//     var angle = playerArcher.body.angle;
-//     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-//     Matter.Body.setAngle(arrow.body, angle);
-//     playerArrows.push(arrow);
-//   }
-// }
-
  function keyPressed() {
-   if (keyCode === 23) {
+    if (keyCode === 32) {
      var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle;
-     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+      var posY = playerArcher.body.position.y;
+      var angle = playerArcher.body.angle;
+      var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
 
-     Matter.Body.setAngle(arrow.body, angle);
+      Matter.Body.setAngle(arrow.body, angle);
      playerArrows.push(arrow);
    }
  }
+
+// function keyPressed() {
+ //  if (keyCode === 23) {
+  //   var posX = playerArcher.body.position.x;
+ //   var posY = playerArcher.body.position.y;
+ //   var angle = playerArcher.body.angle;
+  //   var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+  //   Matter.Body.setAngle(arrow.body, angle);
+  //   playerArrows.push(arrow);
+  // }
+// }
 
 // function keyPressed() {
 //   if (keyCode === 32) {
